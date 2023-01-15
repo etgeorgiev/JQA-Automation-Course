e91278bf75025f75a03bbb8b6ea79a78f9b8fdd4
EGeorgiev <e.tgeorgiev@gmail.com>
2023-01-15 17:26:53 +0200

    Added first task from Homework 1

---
+++ JQALearn/src/main/java/homework1/Task01.java
@@ -0,0 +1,34 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please insert the first number");
+        int firstNumber = scan.nextInt();
+        System.out.println("Your first number is: " + firstNumber);
+
+        System.out.println("Please insert the second number");
+        int secondNumber = scan.nextInt();
+        System.out.println("Your second number is: " + secondNumber);
+
+        System.out.println("Please insert your third number");
+        int thirdNumber = scan.nextInt();
+
+        if (firstNumber > secondNumber) {
+            if (thirdNumber > secondNumber && thirdNumber < firstNumber){
+                System.out.println(thirdNumber + " is between " + secondNumber + " and " + firstNumber);
+            } else {
+                System.out.println(thirdNumber + " isn't between " + secondNumber + " and " + firstNumber);
+            }
+        } else {
+            if (thirdNumber < secondNumber && thirdNumber > firstNumber){
+                System.out.println(thirdNumber + " is between " + firstNumber + " and " + secondNumber);
+            } else {
+                System.out.println(thirdNumber + " isn't between " + firstNumber + " and " + secondNumber);
+            }
+        }
+    }
+}
